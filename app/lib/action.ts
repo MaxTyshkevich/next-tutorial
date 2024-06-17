2c8b93c1053a5b7533baa3e63fa73f6fd4a93418
Max Tyshkevich <2842778@gmail.com>
2024-06-17 13:11:04 +0300

    charter 14: auntification

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,8 +4,9 @@ import { sql } from '@vercel/postgres';
 import { error } from 'console';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-
+import { signIn } from '@/auth';
 import { z } from 'zod';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -116,3 +117,21 @@ export const deleteInvoices = async (id: string) => {
 
   revalidatePath('/dashboard/invoices');
 };
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
